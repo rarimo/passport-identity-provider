c0e80a44635a6cd962d12514419709030eb1dbeb
Artem Skriabin <artem.skriabin@distributedlab.com>
2024-06-25 16:01:17 +0300

    Use current time 2019 year to test old certificate

--- internal/service/api/handlers/create_identity.go
+++ internal/service/api/handlers/create_identity.go
@@ -486,7 +486,8 @@ func validateCert(cert *x509.Certificate, masterCertsPem []byte) error {
 	roots.AppendCertsFromPEM(masterCertsPem)
 
 	foundCerts, err := cert.Verify(x509.VerifyOptions{
-		Roots: roots,
+		Roots:       roots,
+		CurrentTime: time.Unix(1561467563, 0), // Tuesday, June 25, 2019 12:59:23
 	})
 	if err != nil {
 		return fmt.Errorf("invalid certificate: %w", err)
